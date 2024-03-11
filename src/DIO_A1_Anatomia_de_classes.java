5b3a9d5ae063ce6b4e0baab414ffec20b2f52177
Gabriel Setznagl <gabrielembraer3@gmail.com>
2024-03-11 17:59:42 -0300

    First DIO CLass

--- src/DIO_A1_Anatomia_de_classes.java
+++ src/DIO_A1_Anatomia_de_classes.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class DIO_A1_Anatomia_de_classes {
+public class DIO_A1_Anatomia_de_classes {
+    public static void main(String[] args) {
+        System.out.println("Testing simple output");
+
+        String Variavel = "Variable with any string value";
+        final String Constante = "Constat with any string value";
+
+        System.out.println(Variavel);
+        System.out.println(Constante);
+
+    }
 }
