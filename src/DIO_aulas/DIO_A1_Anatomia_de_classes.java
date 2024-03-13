153f62a5350985c5862f0b011079ea019287bd4f
Gabriel Setznagl <91721431+Setznagl@users.noreply.github.com>
2024-03-13 01:41:35 -0300

    refactor(video contents): :construction:

--- src/DIO_aulas/DIO_A1_Anatomia_de_classes.java
+++ src/DIO_aulas/DIO_A1_Anatomia_de_classes.java
@@ -2,11 +2,14 @@ public class DIO_A1_Anatomia_de_classes {
     public static void main(String[] args) {
         System.out.println("Testing simple output");
 
-        String Variavel = "Variable with any string value";
-        final String Constante = "Constat with any string value";
+        String Variavel = "That's a Variable";
+        final String Constante = "That's a constant";
 
         System.out.println(Variavel);
         System.out.println(Constante);
+
+        String Concatenacao = Variavel.concat(" ") + Constante;
+        System.out.println(Concatenacao);
 
     }
 }
