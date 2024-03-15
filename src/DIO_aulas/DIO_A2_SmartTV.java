f59443fb4278aee54bb8dfd1d70431f4e0f94b43
Gabriel Setznagl <91721431+Setznagl@users.noreply.github.com>
2024-03-15 00:49:10 -0300

    Java Class 2 (OPP Principles)

--- src/DIO_aulas/DIO_A2_SmartTV.java
+++ src/DIO_aulas/DIO_A2_SmartTV.java
@@ -1,2 +1,33 @@
-package DIO_aulas;public class DIO_A2_SmartTV {
+package DIO_aulas;
+public class DIO_A2_SmartTV {
+    boolean ligado = false;
+    int canal = 1;
+    int volume = 25;
+
+
+    public void ligar(){
+        ligado = true;
+    }
+
+    public void desligar(){
+        ligado = false;
+    }
+
+    public int aumentarVolume(int volumeAtual){
+        volume ++;
+        System.out.println("volume atual é de: " + volume);
+        return volume;
+    }
+
+    public int diminuirVolume(int volumeAtual){
+        volume --;
+        System.out.println("volume atual é de: " + volume);
+        return volume;
+    }
+
+    public int mudarCanal(int novoCanal){
+        canal = novoCanal;
+        System.out.println("O canal foi alterado para: " + canal);
+        return canal;
+    }
 }
--- src/DIO_aulas/DIO_A2_UsuarioSmartTV.java
+++ src/DIO_aulas/DIO_A2_UsuarioSmartTV.java
@@ -1,2 +1,17 @@
-package DIO_aulas;public class DIO_A2_UsuarioSmartTV {
+package DIO_aulas;
+public class DIO_A2_UsuarioSmartTV {
+    public static void main(String[] args) {
+
+        DIO_A2_SmartTV smartTV = new DIO_A2_SmartTV();
+
+        System.out.println("TV Ligada?" + "  " + smartTV.ligado);
+        System.out.println("Volume" + "  " + smartTV.volume);
+        System.out.println("Canal" + "  " + smartTV.canal);
+
+        smartTV.aumentarVolume(smartTV.volume);
+        smartTV.diminuirVolume(smartTV.volume);
+
+        smartTV.mudarCanal(32);
+
+    }
 }
