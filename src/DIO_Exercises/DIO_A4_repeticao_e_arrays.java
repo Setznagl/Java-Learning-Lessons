5970bbcb0b09e0f92d967a391b3efe3cdd8d930d
Gabriel Setznagl <91721431+Setznagl@users.noreply.github.com>
2024-03-19 23:50:07 -0300

    refactor(DIO_A4_repeticao_e_arrays): :construction: new tests and code refactoring
    create a class to return the higher value and the average of five numbers
    
    work in progress

--- src/DIO_Exercises/DIO_A4_repeticao_e_arrays.java
+++ src/DIO_Exercises/DIO_A4_repeticao_e_arrays.java
@@ -15,14 +15,11 @@ public class DIO_A4_repeticao_e_arrays {
     public static void main(String[] args) {
 
         /*  Bloco Principal de funções
-        ////////////////////////////////
-        ////////////////////////////////
-        ////////////////////////////////
-        ////////////////////////////////
-        ////////////////////////////////
-        //////////////////////////////// */
         scanNomeIdade(escanear.next(), escanear.nextInt());
         notaValida(escanear.nextInt());
+         */
+        maiorE_Media(escanear.nextInt(), escanear.nextInt(), escanear.nextInt(), escanear.nextInt(), escanear.nextInt());
+
 
 
     }
@@ -44,8 +41,7 @@ public class DIO_A4_repeticao_e_arrays {
         System.out.println("Sistema encerrado, nome digitado como 0 ");
     };
 
-    public static void notaValida(int nota1) {
-            System.out.println("Validador de notas");
+    public static int notaValida(int nota1) {
         while (nota1 < 0|| nota1 > 10) {
             System.out.println("A nota digitada foi " + nota1);
             if(nota1 < 0 || nota1 > 10){
@@ -55,10 +51,33 @@ public class DIO_A4_repeticao_e_arrays {
             else { break;};
         }
         System.out.println("A nota digitada é válida e foi : " + nota1);
+        return nota1;
     };
 
-
-
+    public static void maiorE_Media(int nota1, int nota2, int nota3, int nota4, int nota5){
+        System.out.println("As notas selecionadas foram " + nota1 +" , "+ nota2 +" , "+nota3+" , "+nota4+" , "+nota5);
+        int[] notasValidadas = new int[5];
+            notasValidadas[0] = notaValida(nota1);
+            notasValidadas[1] = notaValida(nota2);
+            notasValidadas[2] = notaValida(nota3);
+            notasValidadas[3] = notaValida(nota4);
+            notasValidadas[4] = notaValida(nota5);
+            // i é a variável iteravel similar as usadas em "for"
+            int i = 0;
+            // comparador iniciará com o valor da primeira nota digitada para comparações
+            int comparador = notasValidadas[0];
+                do {
+                    if (i >= notasValidadas.length) break;
+                    else {
+                        if (notasValidadas[i] > comparador){ comparador = notasValidadas[i];}
+                        i++;
+                        System.out.println("O maior número atualmente é : " + comparador);
+                    }
+                }while (i < notasValidadas.length);
+            float mediaNotas = (float) (notasValidadas[0] + (float) notasValidadas[1] + (float) notasValidadas[2] +
+                    (float) notasValidadas[3] + (float) notasValidadas[4]) /notasValidadas.length;
+            System.out.println("A média das notas é de : " + mediaNotas);
+    }
 
 
 
